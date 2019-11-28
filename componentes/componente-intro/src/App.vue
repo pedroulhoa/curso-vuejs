772de7a06276d34d04f50dacc9ea74a22fe2635b
Pedro Ulhoa <pedroulhoa@live.com>
2019-11-28 20:31:32 -0300

    att aulas componentes

--- componentes/componente-intro/src/App.vue
+++ componentes/componente-intro/src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app"> 
-    <h1>Contadores</h1>
-
-    {{contador}}
+    <app-contadores/>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      contador: 0
-    }
-  }
 
 }
 </script>
---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="contadores">
+          <h1>Contadores</h1>
+      <app-contador v-for="c in 5" v-bind:key="c" />
+  </div>
+</template>
+
+<script>
+
+import Contador from './Contador'
+
+export default {
+    components: {
+        'app-contador': Contador,
+    }
+}
+</script>
+
+<style>
+
+</style>
