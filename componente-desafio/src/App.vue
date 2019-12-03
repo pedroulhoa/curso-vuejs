58900e7259b61d089e032e1ce41bbf120078bed8
Pedro Ulhoa <pedroulhoa@live.com>
2019-12-02 21:18:49 -0300

    att desafio componentes

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <sideMenu />
+    <toolbar />
+    <carousel />
+    <footer-bar />
+  </v-app>
+</template>
+
+<script>
+import SideMenu from "./SideMenu";
+import ToolBar from "./ToolBar";
+import Carousel from "./Carousel";
+import Footer from "./Footer";
+
+export default {
+  components: {
+    sideMenu: SideMenu,
+    toolbar: ToolBar,
+    carousel: Carousel,
+    "footer-bar": Footer
+  },
+  data() {
+    return {};
+  }
+};
+</script>
